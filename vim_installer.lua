3b8ba2cf48a80dbce720aff63937d605786a865e
Vftdan <vftdan@gmail.com>
2025-08-03 03:39:00 +0200

    Factor out base url and file list in `vim_installer.lua`
    (Syntax file downloader currently doesn't use base url)

--- vim_installer.lua
+++ vim_installer.lua
@@ -99,53 +99,30 @@ local function download(url, file, noerr)
     fi.close()
 end
 
+local coreFiles = {
+    {"vim.lua"},
+    {".vimrc"},
+    {".version"},
+    {"args.lua", "lib/"},
+    {"file.lua", "lib/"},
+    {"str.lua", "lib/"},
+    {"tab.lua", "lib/"},
+}
+local baseUrl = "https://raw.githubusercontent.com/Minater247/CCVim/main/"
+
 local function install()
     print("Downloading files from github...")
     local oldx, oldy = term.getCursorPos()
     local wid, hig = term.getSize()
-    term.setCursorPos(oldx, oldy)
-    for i=1,wid,1 do
-        term.write(" ")
+    for _, file in ipairs(coreFiles) do
+        term.setCursorPos(oldx, oldy)
+        for i=1,wid,1 do
+            term.write(" ")
+        end
+        term.setCursorPos(oldx, oldy)
+        term.write("Downloading " .. file[1] .. " ...")
+        download(baseUrl .. (file[2] or "") .. file[1], "/vim/" .. (file[2] or "") .. file[1])
     end
-    term.setCursorPos(oldx, oldy)
-    term.write("Downloading vim.lua ...")
-    download("https://raw.githubusercontent.com/Minater247/CCVim/main/vim.lua", "/vim/vim.lua")
-    term.setCursorPos(oldx, oldy)
-    for i=1,wid,1 do
-        term.write(" ")
-    end
-    term.setCursorPos(oldx, oldy)
-    term.write("Downloading .vimrc ...")
-    download("https://raw.githubusercontent.com/Minater247/CCVim/main/.vimrc", "/vim/.vimrc")
-    term.setCursorPos(oldx, oldy)
-    for i=1,wid,1 do
-        term.write(" ")
-    end
-    term.setCursorPos(oldx, oldy)
-    term.write("Downloading .version ...")
-    download("https://raw.githubusercontent.com/Minater247/CCVim/main/.version", "/vim/.version")
-    download("https://raw.githubusercontent.com/Minater247/CCVim/main/lib/args.lua", "/vim/lib/args.lua")
-    term.setCursorPos(oldx, oldy)
-    for i=1,wid,1 do
-        term.write(" ")
-    end
-    term.setCursorPos(oldx, oldy)
-    term.write("Downloading args.lua ...")
-    download("https://raw.githubusercontent.com/Minater247/CCVim/main/lib/fil.lua", "/vim/lib/fil.lua")
-    term.setCursorPos(oldx, oldy)
-    for i=1,wid,1 do
-        term.write(" ")
-    end
-    term.setCursorPos(oldx, oldy)
-    term.write("Downloading str.lua ...")
-    download("https://raw.githubusercontent.com/Minater247/CCVim/main/lib/str.lua", "/vim/lib/str.lua")
-    term.setCursorPos(oldx, oldy)
-    for i=1,wid,1 do
-        term.write(" ")
-    end
-    term.setCursorPos(oldx, oldy)
-    term.write("Downloading tab.lua ...")
-    download("https://raw.githubusercontent.com/Minater247/CCVim/main/lib/tab.lua", "/vim/lib/tab.lua")
     print("Done.")
     print("Do you want to add CCVIM to your universal path?")
     print("This allows you to access it from anywhere on the computer. [y/n]")
@@ -153,7 +130,11 @@ local function install()
     if c == "y" then
         addToPath()
     end
-    if fs.exists("/vim/vim.lua") and fs.exists("/vim/lib/args.lua") and fs.exists("/vim/lib/fil.lua") and fs.exists("/vim/lib/str.lua") and fs.exists("/vim/lib/tab.lua") then
+    local allExist = true
+    for _, file in ipairs(coreFiles) do
+        allExist = allExist and fs.exists("/vim/" .. file[1])
+    end
+    if allExist then
         print("Finished installing.")
         print("Are you able to press tab on your device? [y/n]")
         local _, eh = os.pullEvent("char")
@@ -226,7 +207,7 @@ local function update()
         f = toArr("/vim/.version")
         vimver = tonumber(f[1])
         instver = tonumber(f[2])
-        nf = http.get("https://raw.githubusercontent.com/Minater247/CCVim/main/.version").readAll()
+        nf = http.get(baseUrl .. ".version").readAll()
         nft = split(nf, "\n")
         nvimver = tonumber(nft[1])
         ninstver = tonumber(nft[2])
@@ -235,7 +216,7 @@ local function update()
             print("Download now? [y/n]")
             local _,inp = os.pullEvent("char")
             if inp == "y" then
-                download("https://raw.githubusercontent.com/Minater247/CCVim/main/vim_installer.lua", "/vim/installer")
+                download(baseUrl .. "vim_installer.lua", "/vim/installer")
                 fs.delete("/vim/installer.lua") --legacy installers
                 fs.delete("/vim/vim_installer.lua")
                 fs.move("/vim/installer", "/vim/vim_installer.lua")
@@ -262,11 +243,11 @@ local function update()
             if nvimver > vimver then
                 print("An update is available! "..vimver.." -> "..nvimver)
                 print("Downloading files from github...")
-                download("https://raw.githubusercontent.com/Minater247/CCVim/main/vim.lua", "/vim/vim.lua")
-                download("https://raw.githubusercontent.com/Minater247/CCVim/main/lib/args.lua", "/vim/lib/args.lua")
-                download("https://raw.githubusercontent.com/Minater247/CCVim/main/lib/fil.lua", "/vim/lib/fil.lua")
-                download("https://raw.githubusercontent.com/Minater247/CCVim/main/lib/str.lua", "/vim/lib/str.lua")
-                download("https://raw.githubusercontent.com/Minater247/CCVim/main/lib/tab.lua", "/vim/lib/tab.lua")
+                for _, file in ipairs(coreFiles) do
+                    if not ({[".vimrc"] = true, [".version"] = true})[file[1]] then
+                        download(baseUrl .. (file[2] or "") .. file[1], "/vim/" .. (file[2] or "") .. file[1])
+                    end
+                end
                 print("Update complete.")
                 print("Updating local version info...")
                 local filelines = toArr("/vim/.version")
